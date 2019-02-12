debf41da051a3aae9a9237cbc5457b0c245806fb
alessanderleite <alessander.logistica@gmail.com>
2019-02-12 18:30:24 -0200

    declare and instantiate the variables and objects

--- app/src/main/java/br/com/alessanderleite/moveimages/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/moveimages/MainActivity.java
@@ -1,13 +1,76 @@
 package br.com.alessanderleite.moveimages;
 
+import android.graphics.Point;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Screen Size
+    private int screenWidth;
+    private int screenHeight;
+
+    //Images
+    private ImageView arrowUp;
+    private ImageView arrowDown;
+    private ImageView arrowRight;
+    private ImageView arrowLeft;
+
+    //Position
+    private float arrowUpX;
+    private float arrowUpY;
+    private float arrowDownX;
+    private float arrowDownY;
+    private float arrowRightX;
+    private float arrowRightY;
+    private float arrowLeftX;
+    private float arrowLeftY;
+
+    //Initialize Class
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrowUp = (ImageView) findViewById(R.id.arrowUp);
+        arrowDown = (ImageView) findViewById(R.id.arrowDown);
+        arrowRight = (ImageView) findViewById(R.id.arrowRight);
+        arrowLeft = (ImageView) findViewById(R.id.arrowLeft);
+
+        //Get Screen Size.
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        //Move to out of screen.
+        arrowUp.setX(-80.0f);
+        arrowUp.setY(-80.0f);
+
+        //Start the timer.
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changePos();
+                    }
+                });
+            }
+        }, 0, 20);
+
     }
 }
